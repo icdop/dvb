f03a800cbfbfb8cd67e243c9241d48eb1dbfd9b9
Albert Li <hungchun.li@yahoo.com>
2017-07-26 17:49:34 -0700

    update folder icon in server.js

--- server/routes.js
+++ server/routes.js
@@ -15,7 +15,7 @@
   router.get('/api/tree', function(req, res) {
     var _p;
     if (req.query.id == 1) {
-      _p = path.resolve(__dirname, '..', 'project');
+      _p = path.resolve(__dirname, '..', '.project');
       processReq(_p, res);
 
     } else {
@@ -49,12 +49,12 @@
     var _pf = path.join(_p, f);
     var s = fs.statSync(_pf);
     var sl = fs.lstatSync(_pf);
-    if (sl.isSymbolicLink() == 1) {
+  if (sl.isSymbolicLink() == 1) {
     var _lp = fs.readlinkSync(_pf);
     return {
       "id": _pf,
       "text": f+" @",
-      "icon" :  s.isDirectory() ? 'jstree-custom-folder' : 'jstree-custom-file',
+      "icon" :  s.isDirectory() ? 'jstree-folder' : 'jstree-file',
       "state": {
         "opened": false,
         "disabled": false,
@@ -63,11 +63,30 @@
       "li_attr": {
         "base": _lp,
         "isLink": true,
+        "isDVC" : false,
         "isLeaf": !s.isDirectory()
       },
       "children": s.isDirectory()
     };
-    } else {
+  } else if (fs.existsSync(path.join(_pf,".dvc/README.md"))) {
+    return {
+      "id": _pf,
+      "text": "["+f+"]",
+      "icon" : s.isDirectory() ? 'jstree-custom-folder' : 'jstree-custom-file',
+      "state": {
+        "opened": true,
+        "disabled": false,
+        "selected": false
+      },
+      "li_attr": {
+        "base": _pf,
+        "isLink": false,
+        "isDVC" : true,
+        "isLeaf": !s.isDirectory()
+      },
+      "children": s.isDirectory()
+    };
+  } else {
     return {
       "id": _pf,
       "text": f,
@@ -80,12 +99,12 @@
       "li_attr": {
         "base": _pf,
         "isLink": false,
-        "isDVC" : fs.existsSync(path.join(_pf,".dvc/README.md")),
+        "isDVC" : false,
         "isLeaf": !s.isDirectory()
       },
       "children": s.isDirectory()
     };
-    }
+  }
   }
 
   module.exports = router;
